1fb25b59f0a8f6e4c4d13d14eebb92c471b00a4d
Barry Hanna <barryhanna@users.noreply.github.com>
2021-10-06 07:48:33 +0100

    Site ready to deploy

---
+++ site/src/pages/account/[...].js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { navigate } from 'gatsby-link';
+
+export default function RedirectToAccountDashboard() {
+  React.useEffect(() => {
+    navigate('/account/dashboard', { replace: true });
+  }, []);
+}
